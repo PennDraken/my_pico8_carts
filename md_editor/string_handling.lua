383069e5e5d8025597cbe55f8cac6d73d36e81f8
Henry Strömgren <henry.stromgren@gmail.com>
2025-07-20 09:44:29 +0200

    Buggy cursor snapping
    Todo save final index in row

--- md_editor/string_handling.lua
+++ md_editor/string_handling.lua
@@ -172,7 +172,11 @@ function jump_cursor_down(cursor_index, glyph_rows)
   local glyph = glyph_rows[glyph_row_i][glyph_index_in_row]
   local delta_index = cursor_index - glyph.index_in_text_rows
   local index_in_row = (glyph.index_in_text_rows + delta_index) - glyph_rows[glyph_row_i][1] .index_in_text_rows
-  return index_in_row + glyph_rows[glyph_row_i + 1][1].index_in_text_rows
+  local new_cursor_index = index_in_row + glyph_rows[glyph_row_i + 1][1].index_in_text_rows
+  if new_cursor_index > glyph_rows[glyph_row_i + 2][1].index_in_text_rows then
+    new_cursor_index = glyph_rows[glyph_row_i + 2][1].index_in_text_rows - 1
+  end
+  return new_cursor_index
 end
 
 function jump_cursor_up(cursor_index, glyph_rows)
@@ -181,5 +185,9 @@ function jump_cursor_up(cursor_index, glyph_rows)
   local glyph = glyph_rows[glyph_row_i][glyph_index_in_row]
   local delta_index = cursor_index - glyph.index_in_text_rows
   local index_in_row = (glyph.index_in_text_rows + delta_index) - glyph_rows[glyph_row_i][1] .index_in_text_rows
-  return index_in_row + glyph_rows[glyph_row_i - 1][1].index_in_text_rows
+  local new_cursor_index = index_in_row + glyph_rows[glyph_row_i - 1][1].index_in_text_rows
+  if new_cursor_index > glyph_rows[glyph_row_i - 2][1].index_in_text_rows then
+    new_cursor_index = glyph_rows[glyph_row_i - 2][1].index_in_text_rows - 1
+  end
+  return new_cursor_index
 end
